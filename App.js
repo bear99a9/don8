aa1a5f9c9d0217808b9df565c4dbb5c9cbb54d44
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-03-22 22:27:15 +0000

    Followed tutorial, no error messages, but key aspects still not working

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import * as React from "react";
 import MapView from "react-native-maps";
-import * as Permissions from "expo";
+import * as Permissions from "expo-permissions";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import Polyline from '@mapbox/polyline';
+
+const locations = require('./charitiesLocations.json')
 
 export default class App extends React.Component {
   state = {
     latitude: null,
     longitude: null,
+    locations: locations
   };
   async componentDidMount() {
     const { status } = await Permissions.getAsync(Permissions.LOCATION);
@@ -14,34 +18,89 @@ export default class App extends React.Component {
     if (status !== "granted") {
       const response = await Permissions.askAsync(Permissions.LOCATION);
     }
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, this.mergeCoords),
+      (error) => console.log('Error', error)
+    )
+
+    const { locations: [ sampleLocation ] } = this.state
+
+    this.setState({
+      desLatitude: sampleLocation.coords.latitude,
+      desLongitude: sampleLocation.coords.longitude
+    }, this.mergeCoords)
   }
+
+  mergeCoords = () => {
+   const {
+     latitude,
+     longitude,
+     desLatitude,
+     desLongitude
+   } = this.state
+
+   const hasStartAndEnd = latitude !== null && desLatitude !== null
+
+   if (hasStartAndEnd) {
+     const concatStart = `${latitude},${longitude}`
+     const concatEnd = `${desLatitude},${desLongitude}`
+     this.getDirections(concatStart, concatEnd)
+   }
+ }
+
+ async getDirections(startLoc, desLoc) {
+   try {
+     const resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}`)
+     const respJson = await resp.json();
+     const points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+     const coords = points.map(point => {
+       return {
+         latitude: point[0],
+         longitude: point[1]
+       }
+     })
+     this.setState({ coords })
+   } catch(error) {
+     console.log('Error: ', error)
+   }
+ }
+
   render() {
-    const { latitude, longitude } = this.state;
-    return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude,
-            longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-    );
+    const { latitude, longitude, coords } = this.state
+
+    if (latitude) {
+      return (
+          <MapView
+            showUserLocation
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude,
+              longitude,
+              latitudeDelta: 0.1278,
+              longitudeDelta: 51.5074,
+            }}
+          >
+          <MapView.Polyline
+            strokeWidth={2}
+            strokeColor="red"
+            coordinates={coords}
+          />
+        </MapView>
+      );
+    }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>We need your permission!</Text>
+    </View>
+  )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
